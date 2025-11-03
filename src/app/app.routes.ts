f31eb5c88e6b3940dd9ec7adfa382b04c5a6b73f
BotNicholas <botannicolai22@gmail.com>
2025-11-03 18:30:01 +0200

    Play with Guards a bit...

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {AppComponent1} from './components/app1.component';
+import {canActivate} from './Guards/CanActivate';
+import {HomeComponent} from './components/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: "guard", component: AppComponent1, canActivate: [canActivate] },
+];
---
+++ src/app/components/app1.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+
+@Component({
+  selector: 'app-root1',
+  imports: [FormsModule],
+  standalone: true,
+  templateUrl: './app1.component.html',
+  styleUrl: './app1.component.css'
+})
+export class AppComponent1 {
+
+}
