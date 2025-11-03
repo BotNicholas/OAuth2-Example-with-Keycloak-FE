cc484649a716fe84959bca61c94c6d221dc6ab8b
BotNicholas <botannicolai22@gmail.com>
2025-11-03 18:34:18 +0200

    Revert "Play with Guards a bit..."
    This reverts commit 2b25f5744e286e94fe86a5c19130f873a9413226.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,52 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
 import {FormsModule} from '@angular/forms';
 import {LoginService} from './services/login.service';
+import {TestService} from './services/test.service';
+import {filter, map} from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  standalone: true,
+  // providers: [TestService] <-- this will create a local copy for this component
 })
 export class AppComponent {
-  name = "nick";
-  token:any = "";
-  refresh_token:any = "";
-  id_token:any = "";
+  isReady = false;
 
-  constructor(private loginService: LoginService) { }
-
-  login() {
-    this.loginService.login(this.name).subscribe(
-      res => this.token = res
-    );
+  constructor(private testService: TestService) {
   }
 
-  loginKeyCloak() {
-    // this.loginService.loginKeyCloak().subscribe(
-    //   res => console.log(res)
-    // );
-    let win = window.open("http://192.168.1.5:8080/api/login", '_blank', 'resizable=no, toolbar=no, scrollbars=no, menubar=no, status=no, directories=no, location=no, width=1000, height=600,');
-
-    if (win) {
-      window.addEventListener("message", (event) => {
-        let response = event.data;
-
-        if (event.origin !== "http://192.168.1.5:8080") return;
-        console.log("RECEIVED MESSAGE:")
-        console.log(event);
-
-        this.token = response.access_token;
-        this.refresh_token = response.refresh_token;
-        this.id_token = response.id_token;
-
-        win.close();
-      });
-    }
-  }
-
-  logoutKeyCloak() {
-    this.loginService.logout(this.token, this.id_token).subscribe();
+  ngOnInit() {
+    this.testService.subj.subscribe(res => this.isReady = res);
+    // this.testService.subj.pipe(filter(res => res), map(res => res)).subscribe(res => console.log(res));
   }
 }
