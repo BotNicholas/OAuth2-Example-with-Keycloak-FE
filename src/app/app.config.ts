3eeb27eb3978e3b53c45e87c0b184c5ec5879beb
BotNicholas <botannicolai22@gmail.com>
2025-11-03 18:32:32 +0200

    Revert "Play with Guards a bit..."
    This reverts commit f31eb5c88e6b3940dd9ec7adfa382b04c5a6b73f.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,20 +1,9 @@
-import {
-  APP_INITIALIZER,
-  ApplicationConfig,
-  inject,
-  provideAppInitializer,
-  provideZoneChangeDetection
-} from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import {provideHttpClient} from '@angular/common/http';
-import {TestService} from './services/test.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(), provideAppInitializer(() => {
-    alert("app init")
-    const testService = inject(TestService);
-    testService.testInit();
-  })]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient()]
 };
